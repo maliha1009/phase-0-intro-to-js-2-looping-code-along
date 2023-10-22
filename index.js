44418558a57290dcf928bc6f657ef8e62d3f6603
maliha1009 <90807398+maliha1009@users.noreply.github.com>
2023-10-22 11:34:09 -0400

    solution

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards() {
+    let names = ["Guadalupe", "Ollie", "Aki"];
+    let cards = [];
+    for (let name of names) {
+        cards.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return cards;
+}
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
